d340f242e9ff59681d073631d394641b61209c0a
Lukas Moeller <93522910+42LM@users.noreply.github.com>
2025-08-10 00:43:22 +0200

    fix typos

--- src/utils/commands/help.ts
+++ src/utils/commands/help.ts
@@ -8,14 +8,14 @@ export const help = (
     `<span>&#xf059f; 42LM - Homepage</span><br><br>`,
     `<span is-="text" variant-="green" weight-="bold">Available Commands:</span><br>`,
     // `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">help</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Help about any command</span><br>`, // TODO: implement help for every command e.g. `help switch` ...
-    `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">about</span>&nbsp;&nbsp;&nbsp;&nbsp;Short text about me</span><br>`,
+    `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">whoami</span>&nbsp;&nbsp;&nbsp;Short text about me</span><br>`,
     `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">social</span>&nbsp;&nbsp;&nbsp;Social media links</span><br>`,
     `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">switch</span>&nbsp;&nbsp;&nbsp;Switch dark/light theme</span><br>`,
     `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">clear</span>&nbsp;&nbsp;&nbsp;&nbsp;Clear the screen</span><br><br>`,
     `<span is-="text" variant-="green" weight-="bold">Keyboard Shortcuts:</span><br>`,
     `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">Ctrl+l</span>&nbsp;&nbsp;&nbsp;Clear the screen</span>`,
     `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">Ctrl+u</span>&nbsp;&nbsp;&nbsp;Cut all text from the current cursor position to the beginning of the line</span>`,
-    `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weigth-="bold">Escape</span>&nbsp;&nbsp;&nbsp;Deselect the input field</span>`
+    `<span>&nbsp;&nbsp;<span is-="text" variant-="teal" weight-="bold">Escape</span>&nbsp;&nbsp;&nbsp;Deselect the input field</span>`
     // "<span>Use &quot;[command] help&quot; for more information about a command</span>",
   ];
 
