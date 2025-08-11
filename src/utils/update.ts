b1c7bcaef91a8436d0ad3689d57341b3cf74b354
Lukas Moeller <93522910+42LM@users.noreply.github.com>
2025-08-11 03:11:22 +0200

    create powerline prompt for admin user

--- src/utils/update.ts
+++ src/utils/update.ts
@@ -1,4 +1,6 @@
 const mobileMediaQuery = window.matchMedia('(max-width: 775px)');
+const secretButton = document.getElementById('secret-button') as HTMLInputElement;
+const promptGuest = document.getElementById('prompt-guest') as HTMLInputElement;
 
 // Update the input placeholder of the command line according to screen size.
 export const updateInputPlaceholder = () => {
@@ -17,13 +19,34 @@ export const updatePrompt = () => {
   const promptDomain = document.getElementById('prompt-domain') as HTMLInputElement;
   const promptSigns = document.getElementById('prompt-signs') as HTMLInputElement;
 
-  if (mobileMediaQuery.matches) {
-    promptAt.innerText = '';
-    promptDomain.innerText = '';
-    promptSigns.innerText = ':$';
+  if (secretButton.checked) {
+    if (mobileMediaQuery.matches) {
+      promptGuest.innerHTML = '<span is-="badge" variant-="red" cap-="square"> </span>';
+      promptAt.innerHTML = '';
+      promptDomain.innerHTML = '<span is-="badge" variant-="maroon" cap-="slant-bottom square" style="background-color: var(--red);">42LM.io</span><span is-="badge" variant-="maroon" cap-="square triangle"></span>';
+      promptSigns.innerHTML = '';
+    } else {
+      promptGuest.innerHTML = '<span is-="badge" variant-="red" cap-="square"> </span>';
+      promptAt.innerHTML = '';
+      promptDomain.innerHTML = '<span is-="badge" variant-="maroon" cap-="slant-bottom square" style="background-color: var(--red);">42LM.io</span>';
+      promptSigns.innerHTML = '<span is-="badge" variant-="peach" cap-="slant-bottom square" style="background-color: var(--maroon);">$:</span><span is-="badge" variant-="peach" cap-="square triangle">󰜥 </span>';
+    }
   } else {
-    promptAt.innerText = '@';
-    promptDomain.innerText = '42LM.sh';
-    promptSigns.innerText = ':$ ~';
+    if (mobileMediaQuery.matches) {
+      promptAt.innerText = '';
+      promptDomain.innerText = '';
+      promptSigns.innerText = ':$';
+    } else {
+      promptAt.innerText = '@';
+      promptDomain.innerText = '42LM.sh';
+      promptSigns.innerText = ':$ ~';
+    }
   }
 }
+
+secretButton.addEventListener("click", () => {
+  if (secretButton.checked) {
+    secretButton.style = "background: var(--red);"
+    updatePrompt()
+  }
+});
